bc00e90fc145c8cac804fcc2304548d8d36dba57
Chris Malton <chrism@cmalton.me.uk>
2012-01-14 12:03:37 +0000

    Add an de-authenticate target.

--- server/control.php
+++ server/control.php
@@ -9,6 +9,7 @@ header("Pragma: no-cache");
 header("Content-type: application/json");
 
 $AuthClient = $_SESSION["Client"];
+if($AuthClient == NULL) header("Location: " . dirname($_SERVER["SCRIPT_NAME"]) . "/index.php");
 
 $handle = fopen('php://input','r');
 $jsonInput = fgets($handle);
@@ -32,6 +33,9 @@ switch($_SERVER["PATH_INFO"]){
 			$response["error"] = array(1, "Invalid username or password");
 		}
 		break;
+	case "/auth/deauthenticate":
+		session_destroy();
+		header("Location: " . $AuthClient->GetSetting("LoggedOutURL"));
 }
 
 echo json_encode($response);
