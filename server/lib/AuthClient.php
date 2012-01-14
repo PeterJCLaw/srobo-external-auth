3a1a3ceb62ae823e2ad9b36d8f22a3499be08cfd
Chris Malton <chrism@cmalton.me.uk>
2012-01-14 09:10:26 +0000

    Provide a method to obtain Crypted SSO data.

--- server/lib/AuthClient.php
+++ server/lib/AuthClient.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once("lib/Crypto.php");
+
+class NoPublicKeyException extends Exception { }
+
 class AuthClient {
 
 	private $CONFIG = array();
@@ -39,6 +43,27 @@ class AuthClient {
 		return sha1(microtime());
 	}
 
+	/* 
+	Function: GetSSOData
+	Parameters:
+		None
+	Returns:
+		A crypted string of all the user data as needed.
+		This string is encrypted using the public key of the AuthClient
+	*/
+	public function GetSSOData($username){
+		if($this->GetSetting("PublicKey")){
+			$ap = $this->GetSetting("AuthProvider");
+			$USER_DATA = array(
+						"groups" => $ap->GetGroups($username),
+						"username" => $username,
+						"displayName" => $this->GetUserDisplayName($username),
+					);
+			return base64_encode(Crypto::encryptPublic(json_encode($USER_DATA), $this->GetSetting("PublicKey")));
+		}else{
+			throw new NoPublicKeyException("No public key was given for the AuthClient in use.");
+		}
+	}
 }
 
 ?>
