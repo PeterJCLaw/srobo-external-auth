0013242078e6777ab1621f3a7a33031ae6fb7ab8
Chris Malton <chrism@cmalton.me.uk>
2012-01-14 12:01:36 +0000

    Update default config to give a better example.

--- server/etc/config.default.php
+++ server/etc/config.default.php
@@ -1,16 +1,22 @@
 <?php
-
+require_once("lib/AuthClient.php");
+require_once("lib/FileAuthProvider.php");
 #### COPY THIS FILE TO config.inc.php AND EDIT TO SUIT ####
 
-### Authentication clients
-$_CONFIG["Clients"] = array();
+## Set up some session stuff
+ini_set("session.save_path", dirname(__FILE__) . "/../sessions/");
+ini_set("session.gc_maxlifetime", 3600); //1 hour session time
 
 ## To add an authentication client copy the line below and edit to suit
-# $_CONFIG["Clients"][] = new AuthClient("http://my.url/", "PublicKeyHere", 
-					 "AuthProvider");
 ## http://my.url/ should be replaced with the referer URL, and the Public Key 
 ## (created by genkeypair.php) should be pasted into "PublicKeyHere".
-## AuthProvider must be one of:
-##   none, file, ldap
-
+## AuthProvider an instance of an auth provider
+$client = new AuthClient();
+$client->PutSetting("OriginURL", "http://my.url/");
+$client->PutSetting("LoggedOutURL", "http://my.url/logged_out.php");
+$client->PutSetting("PublicKey", file_get_contents("etc/keys/my_pub_key"));
+$client->PutSetting("AuthProvider", new FileAuthProvider());
+$client->PutSetting("SSO_Title", "Log in name");
+$client->PutSetting("SSO_Subtext", "Log in sub text here");
+ConfigManager::AddClient($client);
 ?>
