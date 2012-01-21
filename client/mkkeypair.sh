f78249d2dd56e13c37b9fcfcc883bcb388b4af39
Chris Malton <chrism@cmalton.me.uk>
2012-01-21 14:45:23 +0000

    Support a filename on the command line, rather than reading from stdin.

--- client/mkkeypair.sh
+++ client/mkkeypair.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
-echo -n "Enter filename for private key:"
-read fileBase 
+fileBase=""
+if [ $# -lt 1 ]; then
+	echo -n "Enter filename for private key:"
+	read fileBase 
+else
+	fileBase="$1"
+fi
 openssl genrsa -out $fileBase 2048
 openssl rsa -in $fileBase -out $fileBase.pub -pubout -outform PEM
